2e36a83ddf7c08641f5ccd76cad57b01dedee08b
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-27 11:09:40 -0500

    guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,29 @@
 function guessingGame() {
+    // initialize random number between 0 and 99
+    const secretNumber = Math.floor(Math.random()*100);
+    let count = 0;
+    let hasWon = false;
 
+    // declare guess function
+    function guessNumber(number) {
+        if (hasWon) {
+            return `The game is over, you already won!`;
+        }
+        count++;
+        // compare to number
+        if (number === secretNumber) {
+            hasWon = true;
+            return `You win! You found ${secretNumber} in ${count} guesses.`;
+        }
+        else if (number > secretNumber) {
+            return `${number} is too high!`;
+        }
+        else {
+            return `${number} is too low!`;
+        }
+    }
+
+    return guessNumber;
 }
 
 module.exports = { guessingGame };
