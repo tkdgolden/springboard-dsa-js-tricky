b29d78a1f903966ee79b39c23ea030ac9f0908a6
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-27 11:37:38 -0500

    curried-add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,18 @@
-function curriedAdd(total) {
+function curriedAdd(initial) {
+    let currentTotal = initial || 0;
 
+    function addTo(number) {
+        if (number) {
+            currentTotal += number;
+            return addTo;
+        }
+        return currentTotal;
+    }
+
+    if (initial) {
+        return addTo;
+    }
+    return currentTotal;
 }
 
 module.exports = { curriedAdd };
